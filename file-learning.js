2fd9d6a825042363e1a38139f5aab67f98f7d3f9
Sachin Boob <sachin.boob@jadeglobal.com>
2020-04-15 18:46:47 +0530

    Adding file-learning.js
    File system APIs

--- file-learning.js
+++ file-learning.js
@@ -5,7 +5,20 @@ const fs = require('fs');
 
 // Variables
 var file_name = 'file1.txt';
+
 // Write to file
-fs.writeFileSync(file_name,'This file ' + file_name + ' is created by writeFileSync function');
-fs.appendFileSync(file_name, '\n');
-fs.appendFileSync(file_name, 'This data is appended to the file by using appendFileSync function');
+fs.writeFileSync(file_name, 'This file ' + file_name + ' is created by writeFileSync function at ' + new Date());
+
+// Append data to file
+fs.appendFileSync(file_name, '\nThis data is appended to the file by using appendFileSync function at ' + new Date());
+
+// Read data from file as a String in synchronous manner
+console.log(fs.readFileSync("./" + file_name, 'utf8', String));
+
+// Read data from file as a String in asynchronous manner
+fs.readFile("./" + file_name, 'utf8', (err, data) => {
+    if (err) {
+        throw err;
+    }
+    console.log(data);
+});
